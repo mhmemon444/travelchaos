842c337c6ad5ba7aea2fdc8d704e864e87e66bc1
Hassan Memon <mhmemon@connect.ust.hk>
2022-11-20 22:56:32 +0000

    docker dev config on server and frontend with dockercompose

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Byess there');
+  res.send('Test with docker');
 });
 
 app.listen(8080, () => {
